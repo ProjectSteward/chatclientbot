3a96e178184fce7085428b52f777fa1e0002945e
Adipat Larprattanakul <adipat.larprattanakul@thomsonreuters.com>
2017-03-24 23:41:42 +0700

    temp

--- app/steward.js
+++ app/steward.js
@@ -1,22 +1,33 @@
 /**
  * Created by Adipat on 3/23/2017.
  */
-import { DirectLine } from 'botframework-directlinejs';
-
 function Steward() {
 
+    var conversations = []; // [{from: "adipat.larprattanakul.thomsonreuters.com@reuters.net", cid: 3}]
+
+    function getConversation(from) {
+        var result = $.grep(conversations, function(e) {
+            return e.from === from;
+        });
+
+        if (result.length > 0)
+            return result[0].cid;
+
+        return null;
+    }
+
     function Ask(from, message) {
-        var directLine = new DirectLine({
+
+        // get
+
+        var directLine = DirectLine({
             secret: '8LcS8tQ0UGU.cwA.iUk._NqOkDxxx37e4d13RzIkxiEci-7WBFqk9c_rZSgzxZM'
         });
 
         directLine.postActivity({
             from: { id: from},
             type: 'message',
-            text: message}).subscribe(
-                id => console.log("Posted activity, assigned ID ", id),
-                error => console.log("Error posting activity", error)
-        );
+            text: message}).sub;
     }
 
     return {
